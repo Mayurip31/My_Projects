e14b041e9737763f8f8bffb2c6d6419417752540
Mayurip31 <mayuripawar6196@gmail.com>
2024-03-05 15:18:46 +0530

    Task 6

--- Multi_Server_Using_Terra.tf
+++ Multi_Server_Using_Terra.tf
@@ -16,7 +16,7 @@ terraform {
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform_locks"
+    #dynamodb_table = "terraform_locks"
   }
 }
 
